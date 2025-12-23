d175f257b549faaa6139379db7de298da13a8c44
Or Barer <or.barer@gmail.com>
2025-12-23 20:39:17 +0200

    instances created

---
+++ main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_security_group" "main" {
+  name = "main_security_group"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "kubernetes_master" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  tags = {
+    Name = "KubernetesMaster"
+  }
+}
+
+resource "aws_instance" "kubernetes_worker" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.main.id]
+  tags = {
+    Name = "KubernetesWorker"
+  }
+}
